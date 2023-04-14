cab3876cb5008ddf7dac113d85165fdafec42178
Faizan Ahemad <ahemf@dev-dsk-ahemf-2b-8c556173.us-west-2.amazon.com>
2023-04-14 11:43:10 +0000

    gpt2-xl with deepspeed

--- scripts/distill_emulator/gpt2-xl-2-base.sh
+++ scripts/distill_emulator/gpt2-xl-2-base.sh
@@ -2,31 +2,32 @@
 MODEL="gpt2-xl"
 student_model_name_or_path="gpt2"
 num_student_layers=6
-bs=2
+bs=4
 pad=2
-
-
 export WANDB_PROJECT="offsite_tuning"
 export WANDB_NAME="${MODEL}_emulator_${num_student_layers}_${pad}_${pad}"
 export WANDB_MODE="dryrun"
 export WANDB_DISABLE_SERVICE=true
+
+rm emulators/${MODEL}/${num_student_layers}_${pad}_${pad}/all_results.json
+
 CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7" accelerate launch \
-    --mixed_precision=fp16 \
+    --mixed_precision=bf16 --multi_gpu \
     offsite_tuning/run_clm.py \
     --model_name_or_path $MODEL \
     --train_tokenized_dataset $HOME/processed_datasets/wikitext_tokenized_blocks \
     --val_tokenized_dataset $HOME/processed_datasets/wikitext_tokenized_blocks \
-    --preprocessing_num_workers 88 \
+    --preprocessing_num_workers 64 \
     --per_device_train_batch_size $((bs * 2)) \
     --per_device_eval_batch_size $((bs * 2)) \
     --gradient_accumulation_steps 4 \
-    --learning_rate 1e-3 \
+    --learning_rate 1e-4 \
     --weight_decay 0.1 \
     --num_warmup_steps 400 \
     --lr_scheduler_type cosine \
     --num_train_epochs 1 \
     --lm_weight 1.0 \
-    --kd_weight 60.0 \
+    --kd_weight 30.0 \
     --seed 37 \
     --block_size 512 \
     --eval_steps 100 \
@@ -35,13 +36,14 @@ CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7" accelerate launch \
     --student_r_pad $pad \
     --train_module "student_patch" \
     --max_train_steps 1200 \
-    --max_train_samples 100000 \
+    --gradient_checkpointing_enable\
     --output_dir emulators/${MODEL}/${num_student_layers}_${pad}_${pad} \
     --report_to wandb \
     --student_model_name_or_path $student_model_name_or_path
 
+
 CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7" accelerate launch \
-    --mixed_precision=fp16 \
+    --mixed_precision=bf16 --multi_gpu\
     offsite_tuning/run_clm.py \
     --model_name_or_path $MODEL \
     --train_tokenized_dataset $HOME/processed_datasets/wikitext_tokenized_blocks \
@@ -49,20 +51,21 @@ CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7" accelerate launch \
     --preprocessing_num_workers 88 \
     --per_device_train_batch_size $bs \
     --per_device_eval_batch_size $bs \
-    --gradient_accumulation_steps 8 \
-    --learning_rate 5e-4 \
+    --gradient_accumulation_steps 4 \
+    --learning_rate 1e-4 \
     --weight_decay 0.1 \
-    --num_warmup_steps 1000 \
+    --num_warmup_steps 2000 \
     --lr_scheduler_type cosine \
-    --num_train_epochs 2 \
+    --num_train_epochs 1 \
     --lm_weight 1.0 \
-    --kd_weight 60.0 \
+    --kd_weight 30.0 \
     --seed 42 \
     --block_size 512 \
-    --eval_steps 100 \
+    --eval_steps 200 \
     --num_student_layers $num_student_layers \
     --student_l_pad $pad \
     --student_r_pad $pad \
+    --gradient_checkpointing_enable \
     --output_dir emulators/${MODEL}/${num_student_layers}_${pad}_${pad} \
     --report_to wandb \
     --student_model_name_or_path $student_model_name_or_path \
--- scripts/distill_emulator/gpt2.sh
+++ scripts/distill_emulator/gpt2.sh
@@ -1,7 +1,7 @@
 # l25
 MODEL="gpt2-xl"
 num_student_layers=16
-bs=10
+bs=8
 pad=2
 
 
@@ -9,17 +9,18 @@ export WANDB_PROJECT="offsite_tuning"
 export WANDB_NAME="${MODEL}_emulator_${num_student_layers}_${pad}_${pad}"
 export WANDB_MODE="dryrun"
 CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7" accelerate launch \
-    --mixed_precision=bf16 --multi_gpu \
+    --mixed_precision=fp16 \
     offsite_tuning/run_clm.py \
-    --model_name_or_path /dataset/gpt/$MODEL \
-    --train_tokenized_dataset /dataset/pile/gpt_tokenized/00 \
-    --val_tokenized_dataset /dataset/gpt_tokenized/wikitext-2-raw-v1 \
+    --model_name_or_path $MODEL \
+    --train_tokenized_dataset $HOME/processed_datasets/wikitext_tokenized_blocks \
+    --val_tokenized_dataset $HOME/processed_datasets/wikitext_tokenized_blocks \
     --preprocessing_num_workers 88 \
+    --gradient_accumulation_steps 4 \
     --per_device_train_batch_size $bs \
     --per_device_eval_batch_size $bs \
     --learning_rate 1e-4 \
     --weight_decay 0.1 \
-    --num_warmup_steps 2000 \
+    --num_warmup_steps 7500 \
     --lr_scheduler_type cosine \
     --num_train_epochs 1 \
     --lm_weight 1.0 \
@@ -30,5 +31,6 @@ CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7" accelerate launch \
     --num_student_layers $num_student_layers \
     --student_l_pad $pad \
     --student_r_pad $pad \
+    --gradient_checkpointing_enable \
     --output_dir emulators/${MODEL}/${num_student_layers}_${pad}_${pad} \
     --report_to wandb
